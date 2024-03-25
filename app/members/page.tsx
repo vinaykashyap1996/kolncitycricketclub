708b1387e3307815a029ad6b161178a91b147365
vinaykashyap1996 <vinay.kashyap234@gmail.com>
2024-03-25 01:07:19 +0100

    fix roles

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -21,6 +21,8 @@ function Members() {
             />
             <div className='px-6 py-4 bg-gray-300'>
               <div className='font-bold text-xl'>Rameez Deshmukh</div>
+              <div className='font-bold text-xl'>Vorsitzender</div>
+
           </div>
         </div>
         <div className='max-w-sm rounded overflow-hidden shadow-lg'>
@@ -32,17 +34,21 @@ function Members() {
             />
             <div className='px-6 py-4 bg-gray-300'>
               <div className='font-bold text-xl'>Vinay Kashyap</div>
+              <div className='font-bold text-xl'>Zweite Vorsitzender</div>
+
           </div>
         </div>
         <div className='max-w-sm rounded overflow-hidden shadow-lg'>
             <Image
-              src='/JD-Singh.jpeg'
+              src='/thumbnail.webp'
               alt='Köln City Cricket Club e.V'
               width={400}
               height={400}
             />
             <div className='px-6 py-4 bg-gray-300'>
               <div className='font-bold text-xl'>Jagandeep Singh</div>
+              <div className='font-bold text-xl'>Secretary</div>
+
           </div>
         </div>
           </div>
@@ -56,6 +62,8 @@ function Members() {
             />
             <div className='px-6 py-4 bg-gray-300'>
               <div className='font-bold text-xl'>Tejas Naik</div>
+              <div className='font-bold text-xl'>Treasurer</div>
+
           </div>
         </div>
         <div className='max-w-sm rounded overflow-hidden shadow-lg'>
@@ -67,17 +75,37 @@ function Members() {
             />
             <div className='px-6 py-4 bg-gray-300'>
               <div className='font-bold text-xl'>Sindhoor Hegde</div>
+              <div className='font-bold text-xl'>Match / Training Manager</div>
+
           </div>
         </div>
         <div className='max-w-sm rounded overflow-hidden shadow-lg'>
             <Image
-              src='/likhon.jpeg'
+              src='/thumbnail.webp'
               alt='Köln City Cricket Club e.V'
               width={400}
               height={400}
             />
             <div className='px-6 py-4 bg-gray-300'>
               <div className='font-bold text-xl'>Ahmed Likhon</div>
+              <div className='font-bold text-xl'>Sports Development Manager</div>
+
+          </div>
+        </div>
+
+       </div>
+       <div className='mt-4 flex flex-row justify-between	'>
+       <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+            <Image
+              src='/thumbnail.webp'
+              alt='Köln City Cricket Club e.V'
+              width={400}
+              height={400}
+            />
+            <div className='px-6 py-4 bg-gray-300'>
+              <div className='font-bold text-xl'>Safi Sangar</div>
+              <div className='font-bold text-xl'>Media Manager</div>
+
           </div>
         </div>
           </div>
